89a3de790bc345055fef2256ff4c320b8002ddd8
Owen L <leggett.owen@gmail.com>
2023-01-02 09:46:32 -0800

    Add database key optimizer (fix #1)

--- python/OptimizeDatabase.py
+++ python/OptimizeDatabase.py
@@ -1,39 +1,32 @@
-"""Write the optimized database file Database.json from the contents of the spreadsheet database stored passed in parameter database"""
+import re, sys
 
-import os, json, re, unicodedata
-from Utils import StrToTimeDelta, Mp3FileName
-from typing import List
-from copy import deepcopy
+def firstLetterUp(string):
+	return string[0].upper() + string[1:]
+def firstLetterLow(string):
+	if string.isupper(): return string
+	return string[0].lower() + string[1:]
 
-def SortDict(d: dict) -> dict:
-    "Return a new dict sorted by its keys."
-    keys = list(d.keys())
-    keys.sort()
-    return {k : d[k] for k in keys}
+def convertKey(key):
+	latinKey = key.replace("ā", "a")
+	words = list(map(firstLetterUp, re.split("[_\- ]", latinKey)))
+	words[0] = firstLetterLow(words[0])
+	return "".join(words)
 
-def AddArguments(parser):
-    "Add command-line arguments used by this module"
-    parser.add_argument('--keyTranslationTable',type=str,default='prototype/OptimizedKeys.json',help='Log of OptimizeDatabase key substitutions; Default: prototype/OptimizedKeys.json')
-    
-gOptions = None
-gDatabase = None
-def main(clOptions,database):
-    """Write the optimized database file Database.json from the contents of the spreadsheet database stored passed in parameter database"""
-    
-    global gOptions
-    gOptions = clOptions
-    
-    optimizedDatabase = deepcopy(database)
-        # A deep copy lets you modify optimizedDatabse in place without touching database, which will be used by subsequent modules
-    
-    # Do nothing for the time being. Owen can write his converter code here.
-    
-    changeLog = {}
-    changeLog = SortDict(changeLog)
-    
-    with open(gOptions.optimizedDatabase, 'w', encoding='utf-8') as file:
-        json.dump(optimizedDatabase, file, ensure_ascii=False, indent=2)
-    
-    with open(gOptions.keyTranslationTable, 'w', encoding='utf-8') as file:
-        json.dump(changeLog, file, ensure_ascii=False, indent=2)
-    
+def stripKey(key):
+	return '"' + convertKey(key.group(1)) + '":'
+
+def convertFile(path, out):
+	f = open(path, "r")
+	lines = []
+	keyPattern = "\"([^\"]+)\"\\s*:"
+	
+	for line in f:
+		convertedLine = re.sub(keyPattern, stripKey, line)
+		lines.append(convertedLine)
+	f.close()
+
+	fw = open(out, "w")
+	fw.writelines(lines)
+	fw.close()
+
+print(convertFile(sys.argv[1], sys.argv[2]))
