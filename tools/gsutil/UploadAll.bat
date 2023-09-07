649ccb40ac99e4eb4c41c3376f08941753b5de21
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-09-07 16:55:29 +0100

    Redirect to pages/index.html; configure upload

--- tools/gsutil/UploadAll.bat
+++ tools/gsutil/UploadAll.bat
@@ -7,10 +7,10 @@ call gsutil -m rsync -r -d audio/excerpts gs://apqs_archive/audio/excerpts
 
 echo Finished first command
 
-call gsutil -m rsync -r -d -x ".*json$" prototype gs://apqs_archive/prototype
+call gsutil -m rsync -r -d -x ".*json$" pages gs://apqs_archive/pages
 
 echo Finished second command
 
-call gsutil cp "index.html" prototype gs://apqs_archive
+call gsutil cp "index.html" gs://apqs_archive
 
 echo Finished third command
