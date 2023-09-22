f46753b96fb44649f46564d711e5bc2eb0fb2f56
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-09-22 15:28:27 +0100

    Finished basic id3 tags

--- python/modules/TagMp3.py
+++ python/modules/TagMp3.py
@@ -4,15 +4,15 @@ We leave the session file tags untouched."""
 from __future__ import annotations
 
 import json, re, os
-import Utils, Render, Alert, Html
+import Utils, Alert
 from typing import Tuple, Type, Callable
-import pyratemp, markdown
 
 import mutagen
+import mutagen.id3
 from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3, TIT2, COMM
+from mutagen.id3 import ID3
 
-def register_comment(desc=''):
+def register_comment(desc='') -> None:
     """Register the comment tag using both UTF-16 and latin encodings.
     Tag choices based on Audacity mp3 encoder.
     Code based on https://www.extrema.is/blog/2021/08/04/comment-and-url-tags-with-mutagen."""
@@ -60,14 +60,79 @@ def PrintID3(tags: ID3) -> None:
     print(tags.pprint())
     print()
 
+removeFromBody = "|".join([r"\{attribution[^}]*}",r"<[^>]*>"])
+def CleanupBody(body:str) -> str:
+    "Remove extraneous bit from the body text in order to make a nice comment tag."
+
+    return re.sub(removeFromBody,"",body)
+
+def ExcerptTags(excerpt: dict) -> dict:
+    """Given an excerpt, return a dictionary of the id3 tags it should have."""
+    event = gDatabase["event"][excerpt["event"]]
+    session = Utils.FindSession(gDatabase["sessions"],excerpt["event"],excerpt["sessionNumber"])
+
+    sessionStr = f", Session {excerpt['sessionNumber']}" if excerpt['sessionNumber'] else ""
+    returnValue = {
+        "title": f"{event['title']}{sessionStr}, Excerpt {excerpt['excerptNumber']}",
+        "artist": [gDatabase["teacher"][t]["fullName"] for t in event["teachers"]],
+        "album": event["title"] + sessionStr,
+        "tracknumber": str(excerpt["excerptNumber"]),
+        "date": str(Utils.ParseDate(session["date"]).year),
+        "comment": CleanupBody(excerpt["body"]),
+        "genre": "Questions and answers",
+        "copyright": "© 2023 Abhayagiri Monastery; not for distribution outside the APQS Archive",
+        "organization": "The Ajahn Pasanno Question and Story Achive",
+        "website": "https://abhayagiri.org/questions/",
+    }
+
+    for key in returnValue:
+        if type(returnValue[key]) == str:
+            returnValue[key] = [returnValue[key]]
+        else:
+            returnValue[key] = ["/".join(returnValue[key])]
+    
+    return returnValue
+
 def AddArguments(parser) -> None:
     "Add command-line arguments used by this module"
-    pass
-    # parser.add_argument('--documentationDir',type=str,default='documentation',help='Read and write documentation files here; Default: ./documenation')
-    
+    parser.add_argument('--forceMp3Tag',action='store_true',help="Always rewrite mp3 tags")
 
 gOptions = None
 gDatabase = None # These globals are overwritten by QSArchive.py, but we define them to keep PyLint happy
+register_comment()
 
 def main() -> None:
-    print("here")
+    if gOptions.excerptMp3 == 'remote':
+        Alert.info.Show("All excerpt mp3 links go to remote servers. No mp3 files will be tagged.")
+        return # No need to run TagMp3 if all excerpt files are remote
+    
+    changeCount = sameCount = 0
+    for x in gDatabase["excerpts"]:
+        if gOptions.events != "All" and x["event"] not in gOptions.events:
+            continue # Only tag mp3 files for the specifed events
+        if not x["fileNumber"]:
+            continue # Ignore session excerpts
+        
+        tags = ExcerptTags(x)
+
+        path = Utils.Mp3Link(x,directoryDepth=0)
+        try:
+            fileTags = EasyID3(path)
+        except mutagen.id3.ID3NoHeaderError:
+            fileTags = mutagen.File(path,easy=True)
+            fileTags.add_tags()
+            Alert.extra.Show("Added tags to",path)
+
+        if tags != dict(fileTags) or gOptions.forceMp3Tag:
+            fileTags.delete()
+            for t in tags:
+                fileTags[t] = tags[t]
+            fileTags.save(v1=2,v2_version=3)
+            changeCount += 1
+            Alert.extra.Show("Updated tags in",path)
+        else:
+            sameCount += 1
+        lastFileTags = fileTags
+    
+    Alert.info.Show("Updated tags in",changeCount,"mp3 files;",sameCount,"files unchaged.")
+
