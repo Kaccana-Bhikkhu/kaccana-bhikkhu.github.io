2980110306f37b68f4e2436e896b1bd5bc58d0d4
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-09-29 15:21:21 +0100

    audio/events -> audio/sessions; WR2015 progress

--- python/modules/SplitMp3.py
+++ python/modules/SplitMp3.py
@@ -23,7 +23,7 @@ def IncludeRedactedExcerpts() -> List[dict]:
 
 def AddArguments(parser):
     "Add command-line arguments used by this module"
-    parser.add_argument('--eventMp3Dir',type=str,default=os.path.join('audio','events'),help='Read session mp3 files from this directory; Default: ./audio/events')
+    parser.add_argument('--eventMp3Dir',type=str,default=os.path.join('audio','sessions'),help='Read session mp3 files from this directory; Default: ./audio/sessions')
     parser.add_argument('--excerptMp3Dir',type=str,default=os.path.join('audio','excerpts'),help='Write excerpt mp3 files from this directory; Default: ./audio/excerpts')
     parser.add_argument('--overwriteMp3',action='store_true',help="Overwrite existing mp3 files; otherwise leave existing files untouched")
 
