70bc6477314ea3a2dede27778b22dcaea89cef8a
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-10-04 16:45:43 +0100

    Move audio files to abhayagiri.org

--- tools/rclone/DraftUpload.bat
+++ tools/rclone/DraftUpload.bat
@@ -1,1 +1,3 @@
-rclone -v copy pages abhayagiri:abhayagiri/media/discs/questions/pages
+rclone -v -P copy --s3-acl=public-read pages abhayagiri:abhayagiri/media/discs/questions/pages
+
+rclone -v -P copy --s3-acl=public-read audio/excerpts abhayagiri:abhayagiri/media/discs/questions/audio/excerpts
