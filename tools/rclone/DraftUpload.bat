0ebe8deb3674a183a81ca8cbc3cf6e0a6d673a52
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-10-05 10:58:50 +0100

    Improved rclone scripts

--- tools/rclone/DraftUpload.bat
+++
@@ -1,3 +0,0 @@
-rclone -v -P copy --s3-acl=public-read pages abhayagiri:abhayagiri/media/discs/questions/pages
-
-rclone -v -P copy --s3-acl=public-read audio/excerpts abhayagiri:abhayagiri/media/discs/questions/audio/excerpts
