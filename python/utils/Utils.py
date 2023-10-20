10db1b4f3de63b28623cac8b7decfebbef18a779
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-10-20 16:47:48 +0100

    Include Django license

--- python/utils/Utils.py
+++ python/utils/Utils.py
@@ -4,13 +4,13 @@ from __future__ import annotations
 
 from datetime import timedelta, datetime
 import copy
-import unicodedata
 import re, os
 from urllib.parse import urlparse
 from typing import List
 import Alert, Link
 import pathlib, posixpath
 from collections.abc import Iterable
+from DjangoTextUtils import slugify,RemoveDiacritics
 
 gOptions = None
 gDatabase:dict[str] = {} # These will be set later by QSarchive.py
@@ -372,25 +372,7 @@ def PairWithSession(excerpts: list[dict],sessions: list[dict]|None = None) -> It
     for session,excerptList in GroupBySession(excerpts,sessions):
         yield from ((session,x) for x in excerptList)
 
-def RemoveDiacritics(string: str) -> str:
-    "Remove diacritics from string."
-    return unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode('ascii')
 
-def slugify(value, allow_unicode=False):
-    """
-    Taken from https://github.com/django/django/blob/master/django/utils/text.py
-    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
-    dashes to single dashes. Remove characters that aren't alphanumerics,
-    underscores, or hyphens. Convert to lowercase. Also strip leading and
-    trailing whitespace, dashes, and underscores.
-    """
-    value = str(value)
-    if allow_unicode:
-        value = unicodedata.normalize('NFKC', value)
-    else:
-        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub(r'[^\w\s-]', '', value.lower())
-    return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 def RegexMatchAny(strings: Iterable[str],capturingGroup = True,literal = False):
     """Return a regular expression that matches any item in strings.
