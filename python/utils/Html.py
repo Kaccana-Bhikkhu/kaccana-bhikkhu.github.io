f009a87debd7c0bea0fd17f2c85b7c284edf5582
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2023-10-29 12:29:37 +0000

    Use string literal for forward type hint

--- python/utils/Html.py
+++ python/utils/Html.py
@@ -125,19 +125,16 @@ class Menu(Renderable):
         for n,item in enumerate(self.items):
             if item.file == itemFileName:
                 self.menu_highlightedItem = n
-    
-class PageDesc: # Define a dummy PageDesc class for the type definitions below
-    pass
 
 # Use Union[] to maintain compatibility with Python 3.9
-PageAugmentorType = Union[str,tuple[PageInfo,str],PageDesc]
+PageAugmentorType = Union[str,tuple[PageInfo,str],"PageDesc"]
 """The acceptable types that can be passed to PageDesc.Augment."""
 
-PageGeneratorMenuItem = Callable[[PageDesc],Iterable[Union[PageInfo,PageDesc]]]
+PageGeneratorMenuItem = Callable[["PageDesc"],Iterable[Union[PageInfo,"PageDesc"]]]
 """Type defintion for a generator function that returns an iterator of pages associated with a menu item.
 See PagesFromMenuGenerators for a full description."""
 
-PageDescriptorMenuItem = Iterable[Union[PageInfo,str,tuple[PageInfo,str],PageDesc]]
+PageDescriptorMenuItem = Iterable[Union[PageInfo,str,tuple[PageInfo,str],"PageDesc"]]
 """An iterable that describes a menu item and pages associate with it.
 It first (optionally) yields a PageInfo object containing the menu title and link. If the first item isn't a PageInfo object, no menu item is generated, but pages are produced as below.
 For each page associated with the menu it then yields one of the following:
