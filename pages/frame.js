bbd0e608b473f31d31c30b3e4417a18a32c152d6
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2024-07-14 16:21:33 +0100

    Restore drilldown buttons by restoring fontawesome stylesheet link

--- pages/frame.js
+++ pages/frame.js
@@ -74,7 +74,7 @@ async function changeURL(pUrl) {
 		.then((r) => pageText(r,pUrl))
 		.then((result) => {
 			let [text, resultUrl] = result
-			text = text.replaceAll(/<link[^>]*rel="stylesheet"[^>]*>/gi,"")
+			text = text.replaceAll(/<link[^>]*rel="stylesheet"[^>]*style\.css[^>]*>/gi,"")
 			frame.innerHTML = text;
 
 			let innerTitle = frame.querySelector("title");
