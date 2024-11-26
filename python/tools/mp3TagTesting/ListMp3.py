a9556e99fa0fcf4c187563d0ead7c37b231c79f1
Kaccana Bhikkhu <121373455+Kaccana-Bhikkhu@users.noreply.github.com>
2024-11-26 13:39:57 +0000

    Finished PSD2023

--- python/tools/mp3TagTesting/ListMp3.py
+++ python/tools/mp3TagTesting/ListMp3.py
@@ -3,7 +3,24 @@
 from __future__ import annotations
 
 import os,sys,argparse
+from datetime import timedelta
 from mutagen.mp3 import MP3
+
+def TimeDeltaToStr(time: timedelta,decimal:bool = False) -> str:
+    """Convert a timedelta object to the form [HH:]MM:SS or [HH:]MM:SS[.sss] if decimal is True."""
+
+    seconds = int(time.total_seconds())
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    decimalPart = f"{0.000001* time.microseconds:f}".strip("0").rstrip(".") if decimal else ""
+
+    if hours:
+        return f"{hours}:{minutes:02d}:{seconds:02d}{decimalPart}"
+    else:
+        return f"{minutes}:{seconds:02d}{decimalPart}"
 
 parser = argparse.ArgumentParser(description="List the properties of mp3 files in a given directory.")
 parser.add_argument('directory',type=str,help="Directory to list mp3 files")
@@ -14,4 +31,5 @@ for fileName in sorted(os.listdir(options.directory)):
     path = os.path.join(options.directory,fileName)
     if fileName.endswith(".mp3"):
         file = MP3(os.path.join(options.directory,fileName))
-        print('\t'.join((fileName,str(file.info.length))))
+        duration = timedelta(seconds=file.info.length)
+        print('\t'.join((fileName,TimeDeltaToStr(duration))))
